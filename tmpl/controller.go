e14055628b410c0aa13d450cacc042560db088fd
Alejandra Diaz <alejandradp19@gmail.com>
2021-09-11 22:45:10 -0500

    Checked that the password is the same as the one in the confirm password field. Now the user can be added with all of the fields in the sign up page. It still looks sad in basic html tho.

--- tmpl/controller.go
+++ tmpl/controller.go
@@ -7,6 +7,7 @@ import "golang.org/x/crypto/bcrypt"
 
 import "net/http"
 
+
 var db *sql.DB
 var err error
 
@@ -18,19 +19,17 @@ func signupPage(res http.ResponseWriter, req *http.Request) {
 
 	username := req.FormValue("username")
 	password := req.FormValue("password")
-	//confirmpassword := req.FormValue("confirmPassword")
+	confirmpassword := req.FormValue("confirmPassword")
 	firstname := req.FormValue("firstname")
 	lastname := req.FormValue("lastname")
+	birthdate := req.FormValue("birthdate")
 
 	var user string
 
-	err := db.QueryRow("SELECT username FROM usersmix WHERE username=?", username).Scan(&user)
+	err := db.QueryRow("SELECT username FROM userswithdate WHERE username=?", username).Scan(&user)
 
-//	if password != confirmpassword{
-//		panic(err.Error())
-//	}
-
-	switch{
+	if password == confirmpassword{
+		switch{
 		case err == sql.ErrNoRows:
 			hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 			if err != nil {
@@ -38,7 +37,7 @@ func signupPage(res http.ResponseWriter, req *http.Request) {
 				return
 			}
 
-			_, err = db.Exec("INSERT INTO usersmix(username, password, firstname, lastname) VALUES(?, ?, ?, ?)", username, hashedPassword,firstname,lastname)
+			_, err = db.Exec("INSERT INTO userswithdate(username, password, firstname, lastname,birthdate) VALUES(?, ?, ?, ?,?)", username, hashedPassword,firstname,lastname,birthdate)
 			if err != nil {
 				http.Error(res, "Server error, unable to create your account.2", 500)
 				return
@@ -52,9 +51,11 @@ func signupPage(res http.ResponseWriter, req *http.Request) {
 		return
 		default:
 			http.Redirect(res, req, "/", 301)
+		}
+	}else{
+		res.Write([]byte("Passwords do not match"))
 	}
 }
-
 
 
 func homePage(res http.ResponseWriter, req *http.Request) {
